a92a367b56504a55265e25fb6bb7771ae0b2a41f
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-02-07 21:00:20 +0200

    [WIP]

--- reddit-migrator-web/src/App.js
+++ reddit-migrator-web/src/App.js
@@ -1,49 +1,73 @@
 import React, { Component } from 'react';
-import { Menu, Icon, Step } from 'semantic-ui-react';
+import { Icon, Step, Grid } from 'semantic-ui-react';
+
+import AuthStep from './steps/auth/AuthStep';
+
+const steps = [
+  {
+    title: 'Authenticate',
+    icon: 'key',
+    description: 'Grant access to your Reddit accounts',
+    component: AuthStep
+  },
+  {
+    title: 'Select Subscriptions',
+    icon: 'checkmark box',
+    description: 'Choose subscriptions to move',
+    component: AuthStep
+  },
+  {
+    title: 'Move!',
+    icon: 'truck',
+    description: 'View progress of moving subscriptions',
+    component: AuthStep
+  }
+];
+
+const initialState = {
+  currentStepIndex: 0,
+  accessTokens: []
+}
 
 class App extends Component {
-
-  handleTitleClick = () => {
-
+  
+  state = {...initialState};
+  
+  handleOnStepCompleted = (data) => {
+    this.setState(() => data);
   }
 
   render() {
+    const {currentStepIndex} = this.state;
+    const ContentComponent = steps[this.state.currentStepIndex].component;
     return (
       <div style={{margin: 10}}>
-        <Menu>
-          <Menu.Item
-            name='Reddit Migrator'
-            active={false}
-            onClick={this.handleTitleClick}
-          />
-        </Menu>
-
-        <Step.Group>
-          <Step>
-            <Icon name='key' />
-            <Step.Content>
-              <Step.Title>Authenticate</Step.Title>
-              <Step.Description>Grant access to your Reddit accounts</Step.Description>
-            </Step.Content>
-          </Step>
-
-          <Step>
-            <Icon name='checkmark box' />
-            <Step.Content>
-              <Step.Title>Select Subscriptions</Step.Title>
-              <Step.Description>Choose subscriptions to move</Step.Description>
-            </Step.Content>
-          </Step>
-
-          <Step>
-            <Icon name='truck' />
-            <Step.Content>
-              <Step.Title>Move!</Step.Title>
-              <Step.Description>View progress of moving subscriptions</Step.Description>
-            </Step.Content>
-          </Step>
-
-        </Step.Group>
+        <Grid>
+          <Grid.Column>
+            <h1>Reddit Migrator</h1>
+            <Step.Group fluid vertical={false} unstackable>
+              {steps.map((step, stepIndex) => 
+                <Step
+                  key={stepIndex}
+                  completed={this.state.currentStepIndex > stepIndex}
+                  active={this.state.currentStepIndex === stepIndex}
+                >
+                  <Icon name={step.icon} />
+                  <Step.Content>
+                    <Step.Title>{step.title}</Step.Title>
+                    <Step.Description>{step.description}</Step.Description>
+                  </Step.Content>
+                </Step>
+              )}
+            </Step.Group>
+            
+            <ContentComponent
+              accessTokens={this.state.accessTokens}
+              onStepCompleted={this.handleOnStepCompleted}
+            />
+            
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
